849c2bfa68c2196d76047efc00bd491055f69a99
kiamfestoh@gmail <kiamfestoh@gmail.com>
2015-04-27 09:49:26 +0300

    migrated tables apartments and bedsitters

--- db/migrate/20150427063834_create_apartments.rb
+++ db/migrate/20150427063834_create_apartments.rb
@@ -1,6 +1,9 @@
 class CreateApartments < ActiveRecord::Migration
   def change
     create_table :apartments do |t|
+    	 t.string :name
+    	  t.string :location
+    	   t.string :noofrooms
 
       t.timestamps null: false
     end
